e917925f3f1d3b30638e07f548f57cfedf1900af
Jacob McKiernan <138262144+j-mckiern@users.noreply.github.com>
2024-07-18 15:21:22 -0400

    Updated About Us Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/HomePage';
+import AboutUs from './pages/AboutUs';
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <div className="App">
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutUs />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/study/:deckId" element={<StudyPage />} />
